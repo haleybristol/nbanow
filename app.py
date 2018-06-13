1f984f7ec41f2fa1ab9b6f9ae664a80985a34c10
gene <efbbrown@gmail.com>
2018-06-13 20:10:25 +0300

    New styles to make the index page look better

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 from flask import Flask, render_template, jsonify, request
 import requests
+import json
+import sys
+
+if sys.platform in ["darwin", "win32"]:
+    run_mode = "local"
+else:
+    run_mode = "production"
 
 app=Flask(__name__)
 
 @app.route("/")
 @app.route("/index/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", run_mode=run_mode)
 
 
 @app.route("/api/stats-scoreboard/")
@@ -16,13 +23,32 @@ def api_stats_scoreboard():
     r = requests.get("https://stats.nba.com/stats/scoreboard/?GameDate=" + game_date + "&LeagueID=00&DayOffset=0", headers=headers)
     return jsonify(r.json())
 
+
 @app.route("/api/game-detail/")
 def api_game_detail():
     gid = request.args.get("gid")
     r = requests.get("https://data.nba.com/data/10s/v2015/json/mobile_teams/nba/2017/scores/gamedetail/" + gid + "_gamedetail.json")
     return jsonify(r.json())
 
+
+@app.route("/test-api/stats-scoreboard/")
+def test_api_stats_scoreboard():
+    game_date = request.args.get("game_date")
+    with open("static/data/stats-scoreboard/game_date=" + game_date.replace("/", "") + ".json", "r") as file:
+        loaded = json.load(file)
+    return jsonify(loaded)
+
+
+@app.route("/test-api/game-detail/")
+def test_api_game_detail():
+    gid = request.args.get("gid")
+    with open("static/data/game-detail/gid=" + gid + ".json", "r") as file:
+        loaded = json.load(file)
+    return jsonify(loaded)
+
+
 @app.errorhandler(404)
 def page_not_found(e):
         return render_template("404.html", title="Page Not Found"), 404
+
 
